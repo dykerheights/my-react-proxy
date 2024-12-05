3d9faa69fcc5a9bc32bd64f41c0f25bb854f8bad
benk <nycstayathome@gmail.com>
2024-12-05 09:30:51 -0500

    add multiple endpoints to proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,20 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/api',
+        '/api1',
         createProxyMiddleware(
             {
                 target: 'http://localhost:8080',
+                changeOrigin: true,
+            }
+        )
+    )
+
+    app.use(
+        '/api2',
+        createProxyMiddleware(
+            {
+                target: 'http://localhost:8080/add-product',
                 changeOrigin: true,
             }
         )
