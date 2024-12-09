f5b38781b320ee1b76f4ab0c8fe99227fb2ff18e
benk <nycstayathome@gmail.com>
2024-12-09 01:09:11 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 
+# React Using 
 <https://create-react-app.dev/docs/proxying-api-requests-in-development/>
+
+* Https: <https://github.com/chimurai/http-proxy-middleware/blob/master/recipes/https.md>
+* Trailing Slash Issue: <https://github.com/chimurai/http-proxy-middleware/issues/1016>
+* Modify Post: <https://github.com/chimurai/http-proxy-middleware/blob/master/recipes/modify-post.md>
+* PUT curl examples: <https://blog.marcnuri.com/curl-put-request-examples>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,21 +2,99 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/api1',
+        '/expressHome',
         createProxyMiddleware(
             {
                 target: 'http://localhost:8080',
                 changeOrigin: true,
+                logger: console,
             }
         )
     )
 
+    /* Dynamic Route Example
+    send:
+    GET http://localhost:3000/expressUser/5
+    Accept: application/json
+
+    hit: "/user/:id"
+    * */
     app.use(
-        '/api2',
+        '/expressUser',
         createProxyMiddleware(
             {
-                target: 'http://localhost:8080/add-product',
+                target: 'http://localhost:8080/user/',
                 changeOrigin: true,
+                logger: console,
+            }
+        )
+    )
+
+    /* Query String Example
+    send:
+    GET http://localhost:3000/expressProfile?name=John&age=25
+    Accept: application/json
+
+    hit: "/profile"
+    * */
+    app.use(
+        '/expressProfile',
+        createProxyMiddleware(
+            {
+                target: 'http://localhost:8080/profile',
+                changeOrigin: true,
+                logger: console,
+            }
+        )
+    )
+
+    /* POST Example
+    send:
+    POST http://localhost:3000/expressEchoPostBody
+    Content-Type: application/json
+
+    {"Product Description":  "good stuff"}
+
+    hit: "/echo-post-body"
+     */
+    app.use(
+        '/expressEchoPostBody',
+        createProxyMiddleware(
+            {
+                target: 'http://localhost:8080/echo-post-body',
+                changeOrigin: true,
+                logger: console,
+            }
+        )
+    )
+
+    /*
+    send: curl -X PUT -d '{"field":"value"}' -H "Content-Type: application/json" localhost:3000/expressUpdateProduct/5
+
+    hit: "/product/:id"
+    * */
+    app.use(
+        '/expressUpdateProduct',
+        createProxyMiddleware(
+            {
+                target: 'http://localhost:8080/product/',
+                changeOrigin: true,
+                logger: console,
+            }
+        )
+    )
+
+    /*
+    send: curl -X DELETE localhost:3000/expressDeleteAccount/25
+    hit: "/account/:id"
+    * */
+    app.use(
+        '/expressDeleteAccount',
+        createProxyMiddleware(
+            {
+                target: 'http://localhost:8080/account/',
+                changeOrigin: true,
+                logger: console,
             }
         )
     )
